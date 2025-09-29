60e1f0236322f43e7052c6643c79da506142a042
wlhd <3509896163@qq.com>
2025-09-29 13:24:47 +0800

    favicon.ico  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   dzweb/.__init__.py.swp
    	new file:   dzweb/routes/.__init__.py.swp
    	modified:   dzweb/routes/__init__.py
    	new file:   dzweb/static/favicon.ico
    	modified:   dzweb/templates/.base.html.swp
    	modified:   dzweb/templates/base.html

--- dzweb/routes/__init__.py
+++ dzweb/routes/__init__.py
@@ -1,5 +1,6 @@
 from flask import send_from_directory
 from . import home, auth, about, product, case, service, human, contact, message, user
+import os
 
 bps = [home, auth, about, product, case, service, human, contact, message, user]
 
@@ -7,3 +8,7 @@ def init_app(app):
     @app.route('/instance-files/<filename>')
     def instance_files(filename):
         return send_from_directory(app.instance_path + '/uploads', filename)
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
