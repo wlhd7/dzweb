4faf48cfce512d7b719f20d66c7d45936181834d
wlhd7 <3509896163@qq.com>
2026-03-03 22:30:47 +0800

    feat(case): Remove sub-page routes and implement main route

--- dzweb/routes/case.py
+++ dzweb/routes/case.py
@@ -1,24 +1,8 @@
 from flask import Blueprint, render_template
 
-
 bp = Blueprint('case', __name__, url_prefix='/case')
 
-
 @bp.route('/')
-def extruder():
-    return render_template('case/extruder.html')
-
-
-@bp.route('/assembly-line')
-def assembly_line():
-    return render_template('case/assembly-line.html')
-
-
-@bp.route('/ass')
-def ass():
-    return render_template('case/ass.html')
-
-
-@bp.route('/robot-welding')
-def robot_welding():
-    return render_template('case/robot-welding.html')
+def main():
+    """主经典案例页面，目前合并为单一空白页。"""
+    return render_template('case/main.html')
