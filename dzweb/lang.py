44417d93bf176f1a4b3ea4dc1c1edf966fdffc34
wlhd7 <3509896163@qq.com>
2026-03-04 08:11:13 +0800

    docs(conductor): Synchronize docs for track '‘经典案例’ (case) 模块化重构'

--- dzweb/lang.py
+++ dzweb/lang.py
@@ -1,16 +1,22 @@
 from flask import session, request, redirect, url_for
-from flask_babel import Babel
+from flask_babel import Babel, get_locale
 
 
 def select_locale():
     lang = request.args.get('lang')
     if lang in ['zh', 'en', 'ja']:
+        session['lang'] = lang
         return lang
     return session.get('lang', 'zh')
 
 
 def init_lang(app):
     Babel(app, locale_selector=select_locale)
+    
+    # Inject get_locale to all templates
+    @app.context_processor
+    def inject_locale():
+        return dict(get_locale=get_locale)
 
     @app.post('/<string:lang>/set-lang')
     def set_lang(lang):
